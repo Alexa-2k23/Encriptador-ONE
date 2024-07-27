9d446a47797283ac97343891b44b0ae83f318db7
Alexa-2k23 <134755591+Alexa-2k23@users.noreply.github.com>
2024-07-27 18:21:55 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -7,29 +7,31 @@ const btnCopy = document.querySelector("#copy_btn");
 
 
 // Configuración inicial de la imagen de fondo en outMessage
-outMessage.style.backgroundImage = "url('./Images/lock.png')";
+outMessage.style.backgroundImage = "url('../Images/lock.png')";
 
 inputVerificar();
 
-// Captura el id en el momento del click y direcciona el programa para el método que encripta el texto
+// Captura la id en el momento del click y direcciona el programa para el método que ENCRIPTA el texto
 btnEncrypt.onclick = (e) => {
     e.preventDefault();
     const encodedTxt = encrypt(input.value);
     outMessage.value = encodedTxt;
     input.value = "";
     outMessage.style.backgroundImage = "none"; // Quitar la imagen de fondo al codificar
+    outMessage.classList.add("focused");
 }
 
-// Captura el id en el momento del click y direcciona el programa para el método que desencripta el texto
+// Captura la id en el momento del click y direcciona el programa para el método que DESENCRIPTA el texto
 btnDecrypt.onclick = (e) => {
     e.preventDefault();
     const decodedTxt = decrypt(input.value);
     outMessage.value = decodedTxt;
     input.value = "";
-    outMessage.style.backgroundImage = "none"; // Quitar la imagen de fondo al decodificar
+    outMessage.style.backgroundImage = "none"; // QuitaR la imagen de fondo al decodificar
+    outMessage.classList.add("focused");
 }
 
-// Captura el id en el momento del click y hace la validación que copia el texto
+// Captura la id en el momento del click y hace la validación que copia el texto
 btnCopy.onclick = (e) => {
     e.preventDefault();
     // Selecciona y copia el texto de outMessage
@@ -38,7 +40,9 @@ btnCopy.onclick = (e) => {
         // Una vez copiado, mueve el texto a txt_input
         input.value = outMessage.value;
         outMessage.value = "";
-        outMessage.style.backgroundImage = "url('./Images/lock.png')"; // Restablecer la imagen de fondo
+        outMessage.style.backgroundImage = "url('../Images/lock.png')"; // Restablecer la imagen de fondo
+        outMessage.classList.remove("focused");
+   
     }).catch(err => {
         console.error('Error al copiar el texto: ', err);
     });
@@ -49,6 +53,7 @@ function encrypt(encodedString) {
     const keyArray = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
     for (let j = 0; j < keyArray.length; j++) {
         encodedString = encodedString.replaceAll(keyArray[j][0], keyArray[j][1]);
+
     }
     return encodedString;
 }
@@ -62,7 +67,7 @@ function decrypt(decodedString) {
     return decodedString;
 }
 
-// Verifica lo ingresado por el usuario
+// Verifica lo ingresado por el usuario y bloquea los caracteres que no sean letras
 function inputVerificar() {
     input.addEventListener("keydown", function (e) {
         const keyCode = (e.keyCode ? e.keyCode : e.which);
@@ -71,4 +76,5 @@ function inputVerificar() {
         }
     });
 }
+
 
